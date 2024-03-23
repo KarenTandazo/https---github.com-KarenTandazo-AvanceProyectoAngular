d91935369984e80e97ee6df4545f50a29876c057
KarenTandazo <148921500+KarenTandazo@users.noreply.github.com>
2024-03-23 11:47:14 -0500

    Ejemplos de creación de validadores de formulario reactivo

--- src/app/paginas/PagVehiculoRegistro/PagVehiculoRegistro.component.ts
+++ src/app/paginas/PagVehiculoRegistro/PagVehiculoRegistro.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Vehiculo } from '../../utilitarios/modelos/detalleAuto';
 import { VehiculoService } from '../../servicios/Vehiculo.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-PagVehiculoRegistro',
@@ -17,25 +17,57 @@ export class PagVehiculoRegistroComponent implements OnInit {
     private formBuilder: FormBuilder
   ) { 
     this.formulario = this.formBuilder.group({
-      "codigo": [],
+      "codigo": ["", [Validators.required, validadorCodigo()]],
+      "codigo_confirm": [],
       "imagen": [],
-      "marca": [],
-      "modelo": [],
+      "marca": ["", [Validators.required]],
+      "modelo": ["", [Validators.minLength(10)]],
       "year": [],
       "color": [],
       "kilometraje": [],
       "precio": [],
       "score": [],
-    });
+    },
+    {
+      validators: compararCodigo(),
+    }
+    );
   }
 
   ngOnInit() {
+    this.formulario.controls["imagen"].disable();
   }
 
   guardar(){
     let vehiculo:Vehiculo = {...this.formulario.value};
     this.vehiculoServicio.addVehiculo(vehiculo);
     console.log("formulario", this.formulario.value);
+    if (this.formulario.valid){
+      alert("Guardado con éxito");
+    }else{
+      alert("Formulario incompleto: Por favor revisa los valores requeridos/obligatorios");
+    }
   }
+}
 
+export function validadorCodigo(): ValidatorFn{
+  return (control: AbstractControl): ValidationErrors | null => {
+    const codigoV = /^\d{4}$/;
+    let value = control.value;
+    if (codigoV.test(value)){
+      return null;
+    }
+    return {"codigoValidate": true};
+  }
+}
+
+export function compararCodigo(){
+  return (formulario: FormGroup): ValidationErrors | null => {
+    let valor = formulario.controls['codigo'].value;
+    let valor2 = formulario.controls['codigo_confirm'].value;
+    if (valor === valor2){
+      return null;
+    }
+    return {"codigoComparativo": true};
+  }
 }
